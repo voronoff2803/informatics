8f2d2e9d2014615c635de53c9c3e10a55b46799f
voronoff <voronoff@MacBook-Pro-Aleksej.local>
2018-06-04 11:31:40 +0300

    fix

--- search.php
+++ search.php
@@ -1,14 +1,13 @@
 <?php
 require_once 'connect.php';
- 
+$full_name = strtr($_GET['full_name'], '*', '%');
+$number = strtr($_GET['number'], '*', '%');
 $connection = mysqli_connect($host, $user, $password, $db);
 echo "<form method='GET' action='search.php'>
 <p>Введите имя ученика: <input type='text' name='full_name' value='$full_name'></p>
 <p>Введите номер ученика: <input type='text' name='number' value='$number'></p>
 <p><input type='submit' name='enter' value='Поиск'></p>
 </form>";
-$full_name = strtr($_GET['full_name'], '*', '%');
-$number = strtr($_GET['number'], '*', '%');
 if (isset($_GET['enter'])) {
     $sql="SELECT pupil_name, pupil_id
     FROM pupils
