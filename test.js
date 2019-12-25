2d768f379f435527334f4dfff393f840aa09250b
Алексей <31672379+voronoff2803@users.noreply.github.com>
2019-12-26 01:07:25 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-alert('hi!')
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+
+
+var times = 10;
+for(var i=0; i < times; i++){
+    var man = 100000 + getRandomInt(150000);
+    console.log(man)
+    sleep(150);
+}
